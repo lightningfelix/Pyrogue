8ca47bf2d53e6a4bfb0ba717bd3bba1a20fc925b
lightningfelix <literalcomp@gmail.com>
2021-03-05 13:28:16 -0600

    Changes before Step 6 starts.

--- entity.py
+++ entity.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import copy
-from typing import Tuple, TypeVar, TYPE_CHECKING
+from typing import Optional, Tuple, TypeVar, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from game_map import GameMap
@@ -9,8 +9,12 @@ if TYPE_CHECKING:
 T = TypeVar("T", bound="Entity")
 
 class Entity:
+
+    gamemap: GameMap
+
     def __init__(
         self,
+        gamemap: Optional[GameMap] = None,
         x: int = 0,
         y: int = 0,
         char: str = "?",
@@ -24,13 +28,26 @@ class Entity:
         self.color = color
         self.name = name
         self.blocks_movement = blocks_movement
+        if gamemap:
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def spawn(self: T, gamemap: GameMap, x: int, y: int) -> T:
         clone = copy.deepcopy(self)
         clone.x = x
         clone.y = y
+        clone.gamemap = gamemap
         gamemap.entities.add(clone)
         return clone
+
+    def place(self, x: int, y: int, gamemap: Optional[GameMap] = None) -> None:
+        self.x = x
+        self.y = y
+        if gamemap:
+            if hasattr(self, "gamemap"):
+                self.gamemap.entities.remove(self)
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def move(self, dx: int, dy:int) -> None:
         self.x += dx
