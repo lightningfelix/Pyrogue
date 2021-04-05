35bbd9cd6f6cbe2e9445335001f1e764ba5728e2
Lightning Felix <literalcomp@gmail.com>
2021-04-05 15:58:43 -0500

    Midway through step 8, before adding the health potion to entity_factory

--- actions.py
+++ actions.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 from typing import Optional, Tuple, TYPE_CHECKING
 
 import color
+import exceptions
 
 if TYPE_CHECKING:
     from engine import Engine
-    from entity import Actor, Entity
+    from entity import Actor, Entity, Item
 
 class Action:
     def __init__(self, entity: Actor) -> None:
@@ -19,6 +20,23 @@ class Action:
 
     def perform(self) -> None:
         raise NotImplementedError()
+
+class ItemAction(Action):
+    def __init__(
+        self, entity: Actor, item: Item, target_xy: Optional[Tuple[int, int]] = None
+    ):
+        super().__init__(entity)
+        self.item = item
+        if not target_xy:
+            target_xy = entity.x, entity.y
+        self.target_xy = target_xy
+
+    @property
+    def target_actor(self) -> Optional[Actor]:
+        return self.engine.game_map.get_actor_at_location(*self.target_xy)
+
+    def perform(self) -> None:
+        self.item.consumable.activate(self)
 
 class EscapeAction(Action):
     def perform(self) -> None:
@@ -54,7 +72,7 @@ class MeleeAction(ActionWithDirection):
     def perform(self) -> None:
         target = self.target_actor
         if not target:
-            return
+            raise exceptions.Impossible("Nothing to attack.")
 
         damage = self.entity.fighter.power - target.fighter.defense
 
@@ -78,11 +96,11 @@ class MovementAction(ActionWithDirection):
         dest_x, dest_y = self.dest_xy
 
         if not self.engine.game_map.in_bounds(dest_x, dest_y):
-            return # Destination is out of bounds.
+            raise exceptions.Impossible("That way is blocked.")
         if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
-            return # Destination is blocked by a tile.
+            raise exceptions.Impossible("That way is blocked.")
         if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
-            return # Destination is blocked by an entity.
+            raise exceptions.Impossible("That way is blocked.")
 
         self.entity.move(self.dx, self.dy)
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import copy
+import traceback
+
 import tcod
 
 import color
@@ -56,7 +58,13 @@ def main() -> None:
             engine.event_handler.on_render(console=root_console)
             context.present(root_console)
 
-            engine.event_handler.handle_events(context)
+            try:
+                for event in tcod.event.wait():
+                    context.convert_event(event)
+                    engine.event_handler.handle_events(event)
+            except Exception:
+                tracebook.print_exc()
+                engine.message_log.add_message(traceback.format_exc(), color.error)
 
 if __name__ == "__main__":
     main()
